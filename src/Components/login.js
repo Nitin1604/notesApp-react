14694096818d7af0f6af40d4415301aa100cda9b
Nitin <80270629+Nitin1604@users.noreply.github.com>
2024-01-19 19:22:10 +0530

    Updated login.js code

--- src/Components/login.js
+++ src/Components/login.js
@@ -6,193 +6,115 @@ import { Helmet } from 'react-helmet';
 
 const Title = 'Login Page';
 const LoginComponent = () => {
+   // validilityMsgInputContact : When inputContact were left empty
+   const [validilityMsgInputContact, setvalidilityMsgInputContact] = useState('')
 
-    // validilityMsgInputName : When inputName were left empty
-    const [validilityMsgInputName, setvalidilityMsgInputName] = useState('')
+   // loginErrorMessage : On clicking login button will show particular error when clicking on a button..
+   const [loginErrorMessage, setLoginErrorMessage] = useState('')
 
-    // validilityMsgInputEmail : When inputEmail were left empty
-    const [validilityMsgInputEmail, setvalidilityMsgInputEmail] = useState('')
-    
-    // validilityMsgInputContact : When inputContact were left empty
-    const [validilityMsgInputContact, setvalidilityMsgInputContact] = useState('')
+   // useState for setting the contact , whenever user left the input field as empty
+   const [inputContact, setInputContact] = useState('')
 
-    // loginErrorMessage : On clicking login button will show particular error when clicking on a button..
-    const [loginErrorMessage, setLoginErrorMessage] = useState('')
+   // function to handle the InputContact
+   function InputContactMsg(event) {
+       setInputContact(event.target.value)
+   }
 
-    // useState for setting the name
-    const [inputName, setInputName] = useState('')
-    
-    
-    // function to handle the InputName
-    function InputNameMsg(event) {
-        setInputName(event.target.value)
-    }
-  
-    // function to handle setInputNameError
-    const setInputNameError = (errorInputName) => {
-        setvalidilityMsgInputName(errorInputName)
-        return false
-    }
-   
-    // useState for setting the email
-    const [inputEmail, setInputEmail] = useState('')
-    
-    // function to handle the InputEmail
-    function InputEmailMsg(event) {
-        setInputEmail(event.target.value)
-    }
-    
-    // function to handle setInputEmailError
-    const setInputEmailError = (errorInputEmail) => {
-        setvalidilityMsgInputEmail(errorInputEmail)
-        return false
-    }
-    
-    // useState for setting the contact
-    const [inputContact, setInputContact] = useState('')
-    
-    // function to handle the InputContact
-    function InputContactMsg(event) {
-        setInputContact(event.target.value)
-    }
-   
-    // function to handle setInputContactError
-    const setInputContactError = (errorInputContact) => {
-        setvalidilityMsgInputContact(errorInputContact)
-        return false
-    }
+   // function to handle setInputContactError when field is empty
+   const setInputContactError = (errorInputContact) => {
+       setvalidilityMsgInputContact(errorInputContact)
+       return false
+   }
 
     // function to handle validation
     const validationFunction = (event) => {
-        event.preventDefault()
+        event.preventDefault();
 
-        // when inputName were left blank 
-        if (inputName === '') {
-            return setInputNameError("Name can't be left blank!!")
-        }
-        else {
-            setvalidilityMsgInputName('')
+        // when inputContact were left blank
+        if (inputContact === '') {
+            return setInputContactError("You forgot to enter your contact number!!");
         }
 
-         // when inputEmail were left blank 
-         if (inputEmail === '') {
-            return setInputEmailError("Email can't be left blank!!")
+        // when inputContact has digits less than 10 , show this error message
+        if (inputContact.length < 10) {
+            return setInputContactError("Contact number must be at least 10 digits long.");
         }
-        else {
-            setvalidilityMsgInputEmail('')
-        }
-      
-         // when inputContact were left blank 
-         if (inputContact === '') {
-            return setInputContactError("You forgot to enter your contact number!!")
-        }
-        else {
-            setvalidilityMsgInputContact('')
-        }
-      
-    }
+
+        // When both conditions are satisfied, clearing the error message
+        setvalidilityMsgInputContact('');
+
+    };
+
     return (
         <>
-            <Helmet>
-                <title>{Title}</title>
-            </Helmet>
-            {/* div having id LoginComponent */}
-            <div id="LoginComponent">
-                <NavigationComponent />
+        <Helmet>
+            <title>{Title}</title>
+        </Helmet>
+        {/* div having id LoginComponent */}
+        <div id="LoginComponent">
+            <NavigationComponent />
 
-                <img src={contactLogo}
-                    className="my-2" alt="Image not found" width="200" height="200"></img>
-                {/* container class */}
-                <div className="container my-4">
+            <img src={contactLogo}
+                className="my-2" alt="Image not found" width="200" height="200"></img>
+            {/* container class */}
+            <div className="container my-4">
 
-                    {/* div tag having form class */}
-                    <div className="form class">
+                {/* div tag having form class */}
+                <div className="form class">
 
-                        <section id="contact">
-                            <h1 className="text-success">Login with your number</h1>
-                            <div id="contact-box">
+                    <section id="contact">
+                        <h1 className="text-success">Login with your number</h1>
+                        <div id="contact-box">
 
-                                <form action="" onSubmit={validationFunction}>
+                            <form action="" onSubmit={validationFunction}>
 
-                                    {/* form-group for "Name" */}
-                                    <div className="form-group">
-                                        <b><label htmlFor="text" className="text-success">Name: </label></b>
-                                        <input type="text" name="text" id="text" placeholder="Enter your name" className="text-primary inputName" value={inputName} onChange={InputNameMsg} />
 
-                                        {/* Validation Block "Input Name" will start here */}
-                                        <div className="row" hidden={validilityMsgInputName === ''}>
-                                            <div className="col-sm-12">
-                                                <div className="row my-2 tx-red">
-                                                    <div className="col-sm-10 tx-center">{validilityMsgInputName}</div>
-                                                </div>
+                                {/* form-group for "Phone Number" */}
+                                <div className="form-group">
+                                    <b><label htmlFor="Name" className="mx-3 text-success">Contact</label></b>
+                                    <input type="text" id="name" name="name" className="col-sm-6 text-primary inputPhone " placeholder="Contact no." value={inputContact} onChange={InputContactMsg} />
+                                    {/* <div>
+                                        {isInvalidContactNumber && <p>Error: Contact number must be 10 digits long.</p>}
+                                    </div> */}
+
+                                    {/* Validation Block "Input Contact" will start here */}
+                                    <div className="row" hidden={validilityMsgInputContact === ''}>
+                                        <div className="col-sm-12">
+                                            <div className="row my-2 tx-red">
+                                                <div className="col-sm-10 tx-center">{validilityMsgInputContact}</div>
                                             </div>
                                         </div>
-                                        {/* Validation Block "Input Name" will ends here */}
-
                                     </div>
-                                    {/* form-group for "Name" */}
+                                    {/* Validation Block "Input Contact" will ends here */}
 
-                                    {/* form-group for "Email"  */}
-                                    <div className="form-group">
-                                        <b><label htmlFor="text" className="text-success">Email: </label></b>
-                                        <input type="text" name="text" id="text" placeholder="Enter your email" className="text-primary inputEmail" value={inputEmail} onChange={InputEmailMsg}/>
+                                </div>
+                                {/* form-group for "Phone Number" */}
 
-                                          {/* Validation Block "Input Email" will start here */}
-                                          <div className="row" hidden={validilityMsgInputEmail === ''}>
-                                            <div className="col-sm-12">
-                                                <div className="row my-2 tx-red">
-                                                    <div className="col-sm-10 tx-center">{validilityMsgInputEmail}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* Validation Block "Input Email" will ends here */}
-                                    </div>
-                                    {/* form-group for "Email"  */}
+                                {/* container class for "Login" button */}
+                                <div className="container" style={{ paddingLeft: '100px' }}>
+                                    <button className="btn btn-success" id="loginID" style={{ borderRadius: '16px', width: '134px', }}>Send OTP</button>
+                                </div>
+                                {/* container class for "Login" button */}
 
-                                    {/* form-group for "Phone Number" */}
-                                    <div className="form-group">
-                                        <b><label htmlFor="text" className="text-success">Phone Number: </label></b>
-                                        <input type="text" name="text" id="text" placeholder="Enter your phone number" className="text-primary inputPhone"value={inputContact} onChange={InputContactMsg} />
+                                {/* this message shown when the particular fields is left blank on clicking login button*/}
+                                <p style={{ color: "red" }}>{loginErrorMessage}</p>
+                                {/* this message shown when the particular fields is left blank on clicking login button*/}
 
-                                         {/* Validation Block "Input Contact" will start here */}
-                                         <div className="row" hidden={validilityMsgInputContact === ''}>
-                                            <div className="col-sm-12">
-                                                <div className="row my-2 tx-red">
-                                                    <div className="col-sm-10 tx-center">{validilityMsgInputContact}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* Validation Block "Input Contact" will ends here */}
-                                          
-                                    </div>
-                                    {/* form-group for "Phone Number" */}
-
-                                    {/* container class for "Login" button */}
-                                    <div className="container">
-                                        <button className="btn btn-success" id="loginID" style={{ borderRadius: '16px', width: '416px', }}>Login</button>
-                                    </div>
-                                    {/* container class for "Login" button */}
-
-                                    {/* this message shown when the particular fields is left blank on clicking login button*/}
-                                    <p style={{ color: "red" }}>{loginErrorMessage}</p>
-                                    {/* this message shown when the particular fields is left blank on clicking login button*/}
-
-                                </form>
-                            </div>
-                        </section>
-
-                    </div>
-                    {/* div tag having form class */}
+                            </form>
+                        </div>
+                    </section>
 
                 </div>
-                {/* container class */}
+                {/* div tag having form class */}
 
             </div>
-            {/* div having id LoginComponent */}
+            {/* container class */}
 
-        </>
+        </div>
+        {/* div having id LoginComponent */}
+    </>
 
-    )
-}
+    );
+};
 
 export default LoginComponent;
